ace701c29adc4cdc6fd57275455a72119a5f944a
Juan Rosero <juan@rosero.dev>
2023-06-11 15:17:31 +0100

    feat: enable image recognition on the API

--- api/app.py
+++ api/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, abort, request
+from PIL import Image
+
+from src.predictions import predict
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/predictions", methods=["POST"])
+def predictions():
+    """Endpoint for predictions. Receives a file and returns the predicted label and probability"""
+    file = request.files["image"]
+    if file.filename is None:
+        abort(400, "No image provided")
+
+    image = Image.open(file.stream)
+
+    return predict(image).to_dict()
