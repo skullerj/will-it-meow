99878359c259f72502a199dc32e8be15141faad3
Juan Rosero <juan@rosero.dev>
2023-06-16 10:46:31 +0100

    feat: return json errors

--- api/app.py
+++ api/app.py
@@ -12,9 +12,16 @@ CORS(app, resources={r"/*": {"origins": ["*"]}})
 def predictions():
     """Endpoint for predictions. Receives a file and returns the predicted label and probability"""
     file = request.files["image"]
-    if file.filename is None:
+
+    if file.filename == "":
         abort(400, "No image provided")
 
     image = Image.open(file.stream)
 
     return predict(image).to_dict()
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    """Error handler for 400 errors"""
+    return {"error": error.description}, 400
